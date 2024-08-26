23e3927d4cbf24d6278c3dc7acf16a9f2b3fbcda
uditha <uditha@intervest.lk>
2024-08-26 12:47:03 +0530

    fork(home) : color changes added

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -12,40 +12,34 @@ import CashComponent from '@/components/CashComponent'
 import MaterialCommunityIcons from '@expo/vector-icons/build/MaterialCommunityIcons'
 
 const Home = () => {
-  const { styles } = useStyles(stylesheet);
-  const data = [{value: 54, color: '#177AD5', text: '54%'},
-    {value: 30, color: '#79D2DE', text: '30%'},
-    {value: 26, color: '#ED6665', text: '26%'},]
+  const { styles, theme } = useStyles(stylesheet);
+  const data = [{ value: 54, color: '#177AD5', text: '54%' },
+  { value: 30, color: '#79D2DE', text: '30%' },
+  { value: 26, color: '#ED6665', text: '26%' },]
   return (
-    <SafeAreaView style={{flex:1,}}>
+    <SafeAreaView style={styles.mainContainer}>
       <Stack.Screen
-        options={{ header: () =>  <View style={{ height:0, justifyContent: 'center', marginTop: 40  }}><Header source={Images.Profile} title='Hi Uditha' /></View> }}
+        options={{ header: () => <View style={{ height: 0, justifyContent: 'center', marginTop: 40 }}><Header source={Images.Profile} title='Hi Uditha' /></View> }}
       />
       <View style={styles.container}>
         <View style={styles.container1Wrap}>
-        <AvailableBalance balance='$3,76800.00'/>
+          <AvailableBalance balance='$3,76800.00' />
         </View>
         <View style={styles.container2Wrap}>
-          <BudgetForMonth budget={'$20345.00'} month='Octomber'/>
+          <BudgetForMonth budget={'$20345.00'} month='Octomber' />
         </View>
         <View style={styles.containerCash}>
-         
-        <Text style={styles.containerCashText}>Income and Expences for Octomber</Text>
+
+          <Text style={styles.containerCashText}>Income and Expences for Octomber</Text>
           <View style={styles.cashContainer}>
-        
-
-             <CashComponent backgroundColor={'#DCE6E5'} ComponentIcon={<MaterialCommunityIcons name="home-analytics" size={24} color="white" />} money='$43555.00' moneyStatus='Income' iconBgRoundedColor='#008000' onPressContainer={()=>alert('lolo')} onPressPlus={()=>alert('pp')}/>
-             <CashComponent backgroundColor={'#C9C6CA'} ComponentIcon={<MaterialCommunityIcons name="purse" size={24} color="white" />} money='$43555.00' moneyStatus='Expences' iconBgRoundedColor='#C5705D' onPressContainer={()=>alert('lolof')} onPressPlus={()=>alert('ppe')}/>
-             
-
-           
-             {/* <CashComponent backgroundColor={'#DAF7A6'}/> */}
+            <CashComponent backgroundColor={theme.colors.light_green} ComponentIcon={<MaterialCommunityIcons name="home-analytics" size={24} color="white" />} money='$43555.00' moneyStatus='Income' iconBgRoundedColor='#008000' onPressContainer={() => alert('lolo')} onPressPlus={() => alert('pp')} />
+            <CashComponent backgroundColor={theme.colors.light_brown} ComponentIcon={<MaterialCommunityIcons name="purse" size={24} color="white" />} money='$43555.00' moneyStatus='Expences' iconBgRoundedColor='#C5705D' onPressContainer={() => alert('lolof')} onPressPlus={() => alert('ppe')} />
           </View>
 
-       
+
         </View>
-      
-       
+
+
       </View>
 
     </SafeAreaView>
@@ -53,46 +47,47 @@ const Home = () => {
 }
 
 const stylesheet = createStyleSheet((theme, runtime) => ({
+  mainContainer: { flex: 1, backgroundColor: 'white' },
   container: {
-    flex:1,
+    flex: 1,
     paddingTop: runtime.screen.height / 60,
-    paddingBottom:  runtime.screen.height / 12,
+    paddingBottom: runtime.screen.height / 12,
     backgroundColor: theme.colors.white
 
   },
-  container1Wrap:{
+  container1Wrap: {
     paddingTop: runtime.screen.height / 50,
-    paddingLeft:runtime.screen.height / 40,
+    paddingLeft: runtime.screen.height / 40,
     paddingRight: runtime.screen.height / 40,
     paddingBottom: runtime.screen.height / 45
   },
-  container2Wrap:{
-    paddingLeft:runtime.screen.height / 40,
+  container2Wrap: {
+    paddingLeft: runtime.screen.height / 40,
     paddingRight: runtime.screen.height / 40,
   },
-  containerCash:{
+  containerCash: {
     paddingTop: runtime.screen.height / 30,
-    paddingLeft:runtime.screen.height / 60,
+    paddingLeft: runtime.screen.height / 60,
     paddingRight: runtime.screen.height / 40,
     paddingBottom: runtime.screen.height / 45,
     backgroundColor: '#F8EDE3',
     marginTop: runtime.screen.height / 30,
-    marginLeft:runtime.screen.height / 30,
+    marginLeft: runtime.screen.height / 30,
     marginRight: runtime.screen.height / 40,
     marginBottom: runtime.screen.height / 45,
     borderRadius: 20
   },
-  cashContainer:{
+  cashContainer: {
     flexDirection: 'row',
-    justifyContent:'space-between',
-    paddingTop: runtime.screen.height/30,
-  
+    justifyContent: 'space-between',
+    paddingTop: runtime.screen.height / 30,
+
   },
-  containerCashText:{
+  containerCashText: {
     fontSize: 13,
-    color:  theme.colors.black,
+    color: theme.colors.black,
     fontFamily: 'Montserrat-Bold',
-    paddingLeft:runtime.screen.height / 60,
+    paddingLeft: runtime.screen.height / 60,
   },
   incomes: {
     gap: 10
@@ -123,8 +118,8 @@ const stylesheet = createStyleSheet((theme, runtime) => ({
     fontSize: 22,
     color: theme.colors.white
   },
-  incomeBlock:{
-    paddingTop:  runtime.screen.height / 60,
+  incomeBlock: {
+    paddingTop: runtime.screen.height / 60,
   }
 }))
 
